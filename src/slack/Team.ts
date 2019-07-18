9889e619a0ce9ec8b388c48590d26780ac9e03fe
david jaramillo <david.jaramillo.c@gmail.com>
2019-07-18 18:37:19 -0400

    add autoreset

---
+++ src/slack/Team.ts
@@ -0,0 +1,27 @@
+import * as winston from "winston";
+const querystring = require("querystring");
+const axios = require('axios');
+
+class TeamMessage {
+	private  URL_MSG:string="https://slack.com/api/chat.postMessage?";
+    private TOKEN: string = process.env.SLACK_TOKEN || null;
+    private CHANNEL: string = process.env.SLACK_CHANNEL || null;
+    
+    async sendMessage(message:string) {
+		try {
+
+            
+            const url:string=`${this.URL_MSG}token=${this.TOKEN}&channel=${this.CHANNEL}&${querystring.stringify({text:message})}&pretty=1`;
+            console.log(url);
+            await axios.get(url);
+
+		} catch (error) {
+			winston.error(`Slack sendMessage error`);
+		}
+		
+	}
+
+
+
+}
+export default TeamMessage;
